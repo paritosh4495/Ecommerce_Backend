2fcdf4da1709645820258a38dfb26e758d81241d
Paritosh Pundlik Pal <paritoshpal7796@gmail.com>
2025-10-22 21:41:53 +0100

    Started writting javadocs

--- ecommerce-yt/src/main/java/com/paritoshpal/ecommerce_yt/controller/AddressController.java
+++ ecommerce-yt/src/main/java/com/paritoshpal/ecommerce_yt/controller/AddressController.java
@@ -12,6 +12,9 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+/**
+ * Controller for managing user addresses.
+ */
 @RestController
 @RequestMapping("/api/address")
 @RequiredArgsConstructor
